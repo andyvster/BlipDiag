4c6f1e7fe56afc4945af634703571e0b00b90845
Andy Vu <andrewvu@Mavericks.local>
2016-10-14 14:07:38 -0700

    Setup

--- BlipDiag/ViewController.swift
+++ BlipDiag/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtLat: UILabel!
+    @IBOutlet weak var txtLong: UILabel!
+    @IBOutlet weak var txtHead: UILabel!
+    @IBOutlet weak var txtMagAcc: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
