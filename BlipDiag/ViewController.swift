284ce99c33f6728c4906fbfd304dff8332b1416f
Andy Vu <andrewvu@Mavericks.local>
2016-10-14 14:14:58 -0700

    Added Delegate Method

--- BlipDiag/ViewController.swift
+++ BlipDiag/ViewController.swift
@@ -8,17 +8,25 @@
 // Adding comment to test git
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var txtLat: UILabel!
     @IBOutlet weak var txtLong: UILabel!
     @IBOutlet weak var txtHead: UILabel!
     @IBOutlet weak var txtMagAcc: UILabel!
     
+    var locationManger: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        locationManger.delegate = self
+        locationManger.requestWhenInUseAuthorization()
+        locationManger.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,6 +34,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        startLocation = newLocation
+        txtLat.text = String(format: "%.4f", startLocation.coordinate.latitude)
+        
+    }
 
 }
 
